6f4fd47ccff1bfe06eb3a2b04d810661f81387c6
Daniel Carlozzi <Daniel.Carlozzi@disney.com>
2022-08-06 19:50:48 -0300

    get single user

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System.Collections.Generic;
+using System.Linq;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -37,9 +38,17 @@ namespace testapicore.Controllers
 
         // GET api/<UsersController>/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public ActionResult<Models.User> Get(int id)
         {
-            return "value";
+            try
+            {
+                return _dbContext.Users.Include(i => i.UserStatus).Single(x => x.Id == id);
+            }
+            catch (System.Exception)
+            {
+                _logger.Log(LogLevel.Error, "Cannot read user {0}", id);
+                return NotFound();
+            }
         }
     }
 }
